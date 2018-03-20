c628f7a6e5fffdc0225e952de2b9ef90b7555f30
cpuccini <carmenpuccini@gmail.com>
2018-03-20 15:17:28 -0500

    Base Code and Offset

--- Current Sensor/currentSensorTest/currentSensorTest.ino
+++ Current Sensor/currentSensorTest/currentSensorTest.ino
@@ -1,9 +1,15 @@
+int value;
+int offset = 510;
+
 void setup() {
   // put your setup code here, to run once:
-
+Serial.begin(9600);   // unsure what 9600 does 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  value = analogRead(A0)-offset;
+  Serial.print(value);
+  Serial.print("\n");
 
 }
