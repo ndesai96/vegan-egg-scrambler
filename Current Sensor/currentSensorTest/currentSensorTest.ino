30a4cf2e4dd5ad22a0dfe6785d5b3305c3f31b11
cpuccini <carmenpuccini@gmail.com>
2018-03-26 20:54:58 -0500

    Running average and current threshold

--- Current Sensor/currentSensorTest/currentSensorTest.ino
+++ Current Sensor/currentSensorTest/currentSensorTest.ino
@@ -2,21 +2,49 @@ int analogPin = 0;
 double raw_value;
 double current;
 double offset = 511.0;
+int motorPin = 3;
+double currentSum = 0.0;
+int runningAvgCt = 10;
+double runningAvg;
+double speed = .5*255; //% of total speed
 
 void setup() {
 
   // start serial output
+  pinMode(motorPin,OUTPUT);
   Serial.begin(9600); // baud rate of 9600
 }
 
 void loop() {
-  // read and adjust raw value reading from sensor
-  raw_value = analogRead(analogPin) - offset;
-  current = (raw_value*.0727) + 0.0195;
+  //start motor 
+  
+  if(speed >=0&&speed<=255);
+    analogWrite(motorPin,speed);
+  for (int i = 0; i < runningAvgCt; i++){
+    // read and adjust raw value reading from sensor
+    raw_value = analogRead(analogPin) - offset;
+    current = (raw_value*.0727) + 0.0195;
+//    Serial.println(current);
+    currentSum = currentSum + current;
+    delay(100);
+  }
+  runningAvg = currentSum / runningAvgCt;
+  Serial.println(runningAvg);
+  currentSum=0;
+  
+  
+   //code current stop
+  if( abs(current)>2.5) { 
+    speed=0;
+  }
+  else {
+    speed=.5*255;
+  }
+    
 
   // print results to serial monitor
-  Serial.println(current);
+  
 
-  // sleep
-  delay(1000);
+
+ 
 }
