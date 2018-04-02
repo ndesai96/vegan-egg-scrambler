7b55b5f29b3b36ac6773f360fd2b0eb1cc7ba4a0
cpuccini <carmenpuccini@gmail.com>
2018-04-01 20:30:32 -0500

    Add exponential filter to original current sensor with new motor

--- Current Sensor/currentSensorTest2/currentSensorTest2.ino
+++ Current Sensor/currentSensorTest2/currentSensorTest2.ino
@@ -1,4 +1,6 @@
 
+#include "Filter.h"
+
 // current sensor analog pin
 const int a0 = 0;
 
@@ -7,11 +9,19 @@ const int pwm = 9;
 
 // default duty cycle
 // motor range 80-255
-const int duty = 128;
+float duty = 175;
 
 // controls motor direction
 const int in1 = 6;
 const int in2 = 7;
+
+int raw_value; // container for analogRead value with offset
+const float raw_offset = 511.0; // experimentally determined DC offset for current sensor
+const float cal_m = 0.727; // calibration coefficient
+const float cal_b = 0.0195; // calibration offset
+ float current=0; //
+
+ExponentialFilter<float>FilteredCurrent(10,0);
 
 void setup() {
   
@@ -24,12 +34,30 @@ void setup() {
   digitalWrite(in1, HIGH);
   digitalWrite(in2, LOW);
 
+
+
 }
 
 void loop() {
 
   analogWrite(pwm, duty);
 
-  Serial.println(analogRead(a0));
+         // read and adjust raw value reading from sensor
+      raw_value = analogRead(a0) - raw_offset;
+      current = (raw_value*cal_m) + cal_b;
+
+  float RawCurrent=current;
+  FilteredCurrent.Filter(RawCurrent);
+  float SmoothCurrent=FilteredCurrent.Current();
+
+  if(abs(SmoothCurrent)>1.1) {
+    duty=0;
+  }
+  else {
+    duty=175;
+  }
+
+  Serial.println(SmoothCurrent);
+  delay(1000);
 
 }
