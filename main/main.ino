54eed88ba42c24293237477000b99cc83bf8b9aa
Neil Desai <ndesai1996@gmail.com>
2018-04-15 17:50:50 -0500

    Added proximity code to main

--- main/main.ino
+++ main/main.ino
@@ -4,15 +4,13 @@
 #include <LiquidCrystal.h>
 //#include <Proximity.h>
 
-
 IRCamera amg;
 
 const int stir_pwm = 4, stir_in1 = 24, stir_in2 = 26; // stir motor
-const int blend_pwm = 5, blend_in1 = 52, blend_in2 = 53, blend_trig = 30); // blender motor
-const int current_sensor = 169, current_data = 0;
+const int blend_pwm = 5, blend_in1 = 52, blend_in2 = 53, blend_trig = 31; // blender motor
+const int current_sensor = 169, current_data = 0; // current sensor
 const int rs = 25, en = 6, d4 = 8, d5 = 27, d6 = 3, d7 = 22; // lcd
-
-// need to double check these pinouts
+const int trig = 23, echo = 29;
 
 Motor stir(stir_pwm, stir_in1, stir_in2, 1000);
 Motor blend(blend_pwm, blend_in1, blend_in2,blend_trig);
@@ -23,20 +21,14 @@ unsigned long timestamp;
 unsigned long stirStartTime;
 unsigned long startTime;
 
-//Proximity prox;
+// proximity parameters
+long duration;
+int distance;
 
 // current sensor parameters
 float current;
 float currentThreshold;
-float unfilCurrent = 0;
-
-//const int curNumReadings = 10; //size of readings array
-//int readings[curNumReadings]; //readings from analog input
-//int curReadIndex = 0; //index of the most recent current reading 
-//int curTotal = 0; //running total
-//int curAverage = 0; //average 
-//int perCurExceedThreshold = 0; //%curent exceed threshold 
-//int currentEnd = 0; 
+float unfilCurrent = 0; 
 
 int weight; 
 float minCurrent;
@@ -54,8 +46,6 @@ bool interferenceOverride = false; // for proximity sensor
 float pixels[AMG88xx_PIXEL_ARRAY_SIZE];
 float averageTemp;
 float tempThreshold;
-
-// proximity sensor parameters
   
 // progress parameters
 int cookProgress = 0; 
@@ -64,16 +54,23 @@ void setup() {
   amg.begin();
   blend.begin();
   Serial.begin(9600);
+
+  digitalWrite(trig, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trig, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig, LOW);
+  duration = pulseIn(echo, HIGH);
+  distance= duration*0.0133/2;
+  lcd.setCursor(0, 0);
+  lcd.print("Distance: ");
+  lcd.print(distance);
+  lcd.print(" in");
   
   // blend for 30 seconds after getting user trigger
   blend.runMotor(100);
   delay(30000);
   blend.stopMotor();
-  
-//  averaging array buffer setup
-//  for(int curThisReading=0; curThisReading < curNumReadings; curThisReading++) {
-//    readings[curThisReading]=0;
-//  }
 }
 
 void loop() {
