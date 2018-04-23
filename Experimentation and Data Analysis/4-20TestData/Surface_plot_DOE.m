b6f09f62fed1fa1bc08e7f5fe058ac6ab14f00f4
cpuccini <carmenpuccini@gmail.com>
2018-04-23 13:13:05 -0500

    Updated surface plots

--- Experimentation and Data Analysis/4-20TestData/Surface_plot_DOE.m
+++ Experimentation and Data Analysis/4-20TestData/Surface_plot_DOE.m
@@ -4,55 +4,58 @@ d1= -1:.1:1;
 d2= -1:.1:1;
 figure 
 %Cook Time: Delay vs Water Temp
-subplot(3,3,1)
+subplot(2,3,1)
 [D1,D2] = meshgrid(d1,d2);
-y = 447.998 + 15.859*D1-30.8151*D2-11.17*D1.*D2;
+y = 0.15667 -0.00839*D1-0.00132*D2+0.008679*D1.*D2;
 surf(D1,D2,y);
+title("Cook Rate: Delay vs Water Temp");
+xlabel("Delay");
+ylabel("Water Temp");
 
 %Cook Time: Delay vs Propeller
-subplot(3,3,2)
+subplot(2,3,2)
 [D1,D2] = meshgrid(d1,d2);
-y = 447.998 + 15.859*D1-77.95*D2-72.5*D1.*D2;
+y = 0.15667 -0.00839*D1+0.03707*D2-.01342*D1.*D2;
 surf(D1,D2,y);
+title("Cook Rate: Delay vs Propeller");
+xlabel("Delay");
+ylabel("Propeller");
+
 
 %Cook Time: Water vs Propeller
-subplot(3,3,3)
+subplot(2,3,3)
 [D1,D2] = meshgrid(d1,d2);
-y = 447.998 + -30.8151*D1-77.95*D2-7.25*D1.*D2;
+y = 0.15667 -0.00132*D1+0.03707*D2-.01079*D1.*D2;
 surf(D1,D2,y);
+title("Cook Rate: Water vs Propeller");
+xlabel("Water");
+ylabel("Propeller");
+
 
 %Final Temp: Delay vs Water
-subplot(3,3,4)
+subplot(2,3,4)
 [D1,D2] = meshgrid(d1,d2);
-y = 63.64 + 1.205*D1+1.344*D2-2.465*D1.*D2;
+y = 66.5046 -2.60756*D1+1.779782*D2-.81431*D1.*D2;
 surf(D1,D2,y);
+title("Temperature: Delay vs Water");
+xlabel("Delay");
+ylabel("Water");
 
 %Final Temp: Delay vs Propeller
-subplot(3,3,5)
+subplot(2,3,5)
 [D1,D2] = meshgrid(d1,d2);
-y = 63.6402 + 1.205*D1-0.2153*D2-2.44*D1.*D2;
+y = 66.50546+ -2.60756*D1-3.689*D2-.15803*D1.*D2;
 surf(D1,D2,y);
+title("Temperature: Delay vs Propeller");
+xlabel("Delay");
+ylabel("Propeller");
 
 %Final Temp: Water vs Propeller
-subplot(3,3,6)
+subplot(2,3,6)
 [D1,D2] = meshgrid(d1,d2);
-y = 63.6402 + 1.344*D1-0.215*D2+0.0734*D1.*D2;
+y = 66.5046 + 1.779782*D1-3.689*D2+3.376498*D1.*D2;
 surf(D1,D2,y);
+title("Temperature: Water vs Propeller");
+xlabel("Water");
+ylabel("Propeller");
 
-%Material Loss: Delay vs Water
-subplot(3,3,7)
-[D1,D2] = meshgrid(d1,d2);
-y = 69 + -.75*D1+-4.75*D2+2*D1.*D2;
-surf(D1,D2,y);
-
-%Material Loss: Delay vs Propeller
-subplot(3,3,8)
-[D1,D2] = meshgrid(d1,d2);
-y = 69 + -.75*D1-5*D2+2.75*D1.*D2;
-surf(D1,D2,y);
-
-%Material Loss: Water vs Propeller
-subplot(3,3,9)
-[D1,D2] = meshgrid(d1,d2);
-y = 69 + -4.75*D1-5*D2+-3,75*D1.*D2;
-surf(D1,D2,y);
