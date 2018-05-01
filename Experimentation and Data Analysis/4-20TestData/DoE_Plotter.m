9a128ea6c6ed5a000e05b2b49c98631bcbeb9bcf
Siddharth Kurwa <skurwa@utexas.edu>
2018-04-30 21:38:10 -0500

    DOE data analysis

--- Experimentation and Data Analysis/4-20TestData/DoE_Plotter.m
+++ Experimentation and Data Analysis/4-20TestData/DoE_Plotter.m
@@ -8,18 +8,18 @@ subplot(2,3,1)
 [D1,D2] = meshgrid(d1,d2);
 y = 0.15667 -0.00839*D1-0.00132*D2+0.008679*D1.*D2;
 surf(D1,D2,y);
-title("Cook Rate: Delay vs Water Temp");
-xlabel("Delay");
-ylabel("Water Temp");
+title('Cook Rate: Delay vs Water Temp');
+xlabel('Delay');
+ylabel('Water Temp');
 
 %Cook Time: Delay vs Propeller
 subplot(2,3,2)
 [D1,D2] = meshgrid(d1,d2);
 y = 0.15667 -0.00839*D1+0.03707*D2-.01342*D1.*D2;
 surf(D1,D2,y);
-title("Cook Rate: Delay vs Propeller");
-xlabel("Delay");
-ylabel("Propeller");
+title('Cook Rate: Delay vs Propeller');
+xlabel('Delay');
+ylabel('Propeller');
 
 
 %Cook Time: Water vs Propeller
@@ -27,9 +27,9 @@ subplot(2,3,3)
 [D1,D2] = meshgrid(d1,d2);
 y = 0.15667 -0.00132*D1+0.03707*D2-.01079*D1.*D2;
 surf(D1,D2,y);
-title("Cook Rate: Water vs Propeller");
-xlabel("Water");
-ylabel("Propeller");
+title('Cook Rate: Water vs Propeller');
+xlabel('Water');
+ylabel('Propeller');
 
 
 %Final Temp: Delay vs Water
@@ -37,25 +37,25 @@ subplot(2,3,4)
 [D1,D2] = meshgrid(d1,d2);
 y = 66.5046 -2.60756*D1+1.779782*D2-.81431*D1.*D2;
 surf(D1,D2,y);
-title("Temperature: Delay vs Water");
-xlabel("Delay");
-ylabel("Water");
+title('Temperature: Delay vs Water');
+xlabel('Delay');
+ylabel('Water');
 
 %Final Temp: Delay vs Propeller
 subplot(2,3,5)
 [D1,D2] = meshgrid(d1,d2);
 y = 66.50546+ -2.60756*D1-3.689*D2-.15803*D1.*D2;
 surf(D1,D2,y);
-title("Temperature: Delay vs Propeller");
-xlabel("Delay");
-ylabel("Propeller");
+title('Temperature: Delay vs Propeller');
+xlabel('Delay');
+ylabel('Propeller');
 
 %Final Temp: Water vs Propeller
 subplot(2,3,6)
 [D1,D2] = meshgrid(d1,d2);
 y = 66.5046 + 1.779782*D1-3.689*D2+3.376498*D1.*D2;
 surf(D1,D2,y);
-title("Temperature: Water vs Propeller");
-xlabel("Water");
-ylabel("Propeller");
+title('Temperature: Water vs Propeller');
+xlabel('Water');
+ylabel('Propeller');
 
--- Experimentation and Data Analysis/consistencyAnalysis.m
+++ Experimentation and Data Analysis/consistencyAnalysis.m
@@ -4,47 +4,66 @@
 clear;
 
 % get data for analysis
-filename = '4-06TestData/test3.csv';
+filename = '4-20TestData/test7_run1.csv';
 M = csvread(filename);
 
 % separate raw data into different arrays
 dimM = size(M);
-rawCurrent = M(:,1);
+time = (M(:,1) - M(1,1)) / 1000;
+currentConsistency = M(:,2);
+check = M(:,3); % use the third column to check if current or consistency value;
 
-% % % ignore outlier data points
-% % ctr = 1;
-% % for ii = 1:1:dimM(1)
-% %     if rawCurrent(ii) < .5 && rawCurrent(ii) > .05
-% %           reducedCurrent(ctr) = rawCurrent(ii);
-% %           ctr = ctr + 1;
-% %     end
-% % end
-% % 
-% % % exponential filtering
-% % weight = .9;
-% % expFilteredCurrent = zeros(1, length(reducedCurrent) - 1);
-% % for ii = 1:1:length(expFilteredCurrent)
-% %     expFilteredCurrent(ii) = reducedCurrent(ii)*(1-weight) + reducedCurrent(ii + 1)*(weight);
-% % end
-% % 
-% % % butterworth filtering
-% % [b, a] = butter(6, .7, 'low');
-% % butterFilteredCurrent = filter(b, a, reducedCurrent);
-% % 
-% % % averaging 
-% % avgCount = 5;
-% % ctr = 1;
-% % while ctr*avgCount < length(reducedCurrent)
-% %     avgCurrent(ctr) = 0;
-% %     for ii = 1:1:avgCount
-% %         avgCurrent(ctr) = avgCurrent(ctr) + reducedCurrent((ctr - 1)*avgCount + ii);
-% %     end
-% %     avgCurrent(ctr) = avgCurrent(ctr) / avgCount;
-% %     ctr = ctr + 1;
-% % end
-% % % polyfit - hard to implement in realtime
-% % p = polyfit(1:1:length(reducedCurrent), reducedCurrent, 1);
-% % linCurrent = p(1)*(1:1:length(reducedCurrent)) + p(2);
+curCtr = 1;
+conCtr = 1;
+for ii = 1:1:dimM(1)
+    if check(ii) > 0 
+        currentTime(curCtr) = time(ii);
+        rawCurrent(curCtr) = currentConsistency(ii);
+        curCtr = curCtr + 1;
+    else
+        consistencyTime(conCtr) = time(ii);
+        consistency(conCtr) = currentConsistency(ii);
+        conCtr = conCtr + 1;
+    end
+end
+
+        
+
+% ignore outlier data points
+ctr = 1;
+for ii = 1:1:length(rawCurrent)
+    if rawCurrent(ii) < .5 && rawCurrent(ii) > .05
+          reducedTime(ctr) = currentTime(ii);
+          reducedCurrent(ctr) = rawCurrent(ii);
+          ctr = ctr + 1;
+    end
+end
+
+% exponential filtering
+weight = .6;
+expFilteredCurrent = zeros(1, length(reducedCurrent) - 1);
+for ii = 1:1:length(expFilteredCurrent)
+    expFilteredCurrent(ii) = reducedCurrent(ii)*(1-weight) + reducedCurrent(ii + 1)*(weight);
+end
+
+% butterworth filtering
+[b, a] = butter(6, .2, 'low');
+butterFilteredCurrent = filter(b, a, reducedCurrent);
+
+% averaging 
+avgCount = 5;
+ctr = 1;
+while ctr*avgCount < length(reducedCurrent)
+    avgCurrent(ctr) = 0;
+    for ii = 1:1:avgCount
+        avgCurrent(ctr) = avgCurrent(ctr) + reducedCurrent((ctr - 1)*avgCount + ii);
+    end
+    avgCurrent(ctr) = avgCurrent(ctr) / avgCount;
+    ctr = ctr + 1;
+end
+% polyfit - hard to implement in realtime
+p = polyfit(1:1:length(reducedCurrent), reducedCurrent, 1);
+linCurrent = p(1)*(1:1:length(reducedCurrent)) + p(2);
 
 % adding a "pseudo-time" dependence
 rawCurrentLen = length(rawCurrent);
@@ -52,7 +71,7 @@ ctr = 1;
 conCtr = 1;
 minCurrent = 0;
 maxCurrent = 0;
-while (ctr + 5 < rawCurrentLen)
+while (ctr + 10 < rawCurrentLen)
     if rawCurrent(ctr) > .05 && rawCurrent(ctr) < .5
         for ii = 1:1:10
                 if ii == 1
@@ -67,13 +86,13 @@ while (ctr + 5 < rawCurrentLen)
         ctr = ctr - 1;
     end
         ctr = ctr + 1;
-        consistency(conCtr) = minCurrent*(1 - (ctr/300)) + maxCurrent*(ctr/300);
+        consistency(conCtr) = minCurrent*(1 - (time(ctr)/300)) + maxCurrent*(time(ctr)/300);
         conCtr = conCtr + 1;
         
 end
 
 % exponentially filtered consistency 
-weight = .3;
+weight = .6;
 for ii = 2:1:length(consistency)
     consistency(ii) = consistency(ii)*weight + consistency(ii-1)*(1-weight);
 end
@@ -81,11 +100,13 @@ end
     
         
 
-plotCtr = 5;
-figure(1);
+% plotCtr = 1;
+% figure(1);
 % subplot(plotCtr,1,1);
-% plot(1:1:length(rawCurrent), rawCurrent);
-% title(strcat('raw current','-', filename));
+% plot(reducedTime(2:end), expFilteredCurrent);
+% title(strcat('exponentially-filtered current vs time'));
+% xlabel('time (seconds)');
+% ylabel ('exp-filtered current (A)');
 % subplot(plotCtr,1,2);
 % plot(1:1:length(reducedCurrent), reducedCurrent);
 % title(strcat('reduced current','-', filename));
@@ -93,8 +114,34 @@ figure(1);
 % plot(1:1:length(avgCurrent), avgCurrent)
 % title(strcat('averaging current','-', filename));
 % subplot(plotCtr,1,3);
-plot(1:1:conCtr - 1,consistency)
-title(strcat('time dependent consistency','-', filename));
+
+% testCurrent = .11 + .09*rand(5*length(consistencyTime), 1);
+% ctr = 0;
+% consistency = zeros(1, length(consistencyTime));
+% conCtr = 2;
+% while ctr + 5 < length(testCurrent)
+%     maxCurrent = 0;
+%     minCurrent = 10;
+%     for i = 1:1:5
+%         maxCurrent = max(testCurrent(i+ctr), maxCurrent);
+%         minCurrent = min(testCurrent(i + ctr), minCurrent);
+%     end
+%     ctr = ctr + 5;
+%     consistency(conCtr) = consistency(conCtr - 1)*(1-.6) + (minCurrent*(1-(8*conCtr/300)) + maxCurrent*(8*conCtr/300))*.6;
+%     conCtr = conCtr + 1;
+% end
+
+
+plot(consistencyTime,consistency)
+title(strcat('consistency vs time - stove off'));
+xlabel('time (seconds)');
+ylabel('consistency');
+hold on;
+consistencyThreshold = zeros(1,length(consistency)) + .26;
+plot(consistencyTime, consistencyThreshold);
+legend('consistency', 'threshold');
+hold off;
+
 % subplot(plotCtr,1,plotCtr);
 % plot(1:1:length(linCurrent), linCurrent);
 % title(strcat('linear fit','-', filename));
